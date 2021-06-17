486ed47e2a1fedc920d99da2a3fff672eb5cd2c2
Dennis Kleber <dennis_kleber@mailbox.org>
2021-06-17 11:32:02 +0200

    add donation-backend

--- admin/dev/email/init.sh
+++ admin/dev/email/init.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-create_address() {
-    curl -X POST -H "Content-Type: application/json" -d @${1} http://localhost:1337/admin/email/email
-}
-create_job() {
-    curl -X POST -H "Content-Type: application/json" -d @${1} http://localhost:1337/admin/email/job
+create() {
+    curl -X POST -H "Content-Type: application/json" -d @address.json http://localhost:1337/admin/email/email
+    #curl -X POST -H "Content-Type: application/json" -d @participation.default.json http://localhost:1337/admin/email/job
 }
 
-create_address address.json &&
-create_job register.default.json &&
-create_job resetpw.default.json &&
-create_job resetconfirm.default.json
+create
 
